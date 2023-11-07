578acb2459859b8923623056430a7fff1177732b
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-11-07 16:32:02 +0100

    Preparation des classes Rent et Search

--- src/Search.java
+++ src/Search.java
@@ -1,14 +1,37 @@
-
-//Ici ou dans un fichier à part
+import java.util.ArrayList;
 
 public class Search {
 
-    TypeSearch keyword;
+    EnumTypeSearch keywordType;
 
     String keyword;
 
-    //Tu vas avoir des probs si tu la déclares ici
+    public enum EnumTypeSearch {
+        ACTOR, DIRECTOR, MOVIE_NAME, UNKNOWN 
+    }
 
-    //Essaye de push déjà tout ce bordel pour qu'Esteban puisse écrire en local, il a 50sec de latence
+    public Search(String keyword, EnumTypeSearch keywordType) {
+        this.keyword = keyword;
+        this.keywordType = keywordType;
+    }
+
+    // Méthode pour la recherche : à voir si on conserve la classe entière
+    public ArrayList<Movie> search(ArrayList<Movie> movies) {
+        ArrayList<Movie> result = new ArrayList<Movie>();
+        for (Movie movie : movies) {
+            if (movie.getMainActors().contains(keyword)) {
+                result.add(movie);
+            }
+            else if (movie.getDirector().equals(keyword)) {
+                result.add(movie);
+            }
+            else if (movie.getName().equals(keyword)) {
+                result.add(movie);
+            }
+        }
+        return result;
+    }
+
+
 
 }
