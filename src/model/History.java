3d93e7cd518cb2a49fd69dfe970ccd66a929564e
Antoine Sauzeau <antoine.sauzeau3@gmail.com>
2023-11-14 16:44:22 +0100

    Gros gros travail !

---
+++ src/model/History.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class History {
+    private ArrayList<Rent> RentalList;
+
+
+    void updateHistory(Client c){
+        //Fait la requete de la base
+    }
+}
