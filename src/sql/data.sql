0d61c59f37dc28a5668ffe663eea9a2f8aa6236e
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-11-21 16:29:31 +0100

    Mise à jour de la BD

--- src/sql/data.sql
+++ src/sql/data.sql
@@ -14,13 +14,13 @@ INSERT INTO NonSubscribers (clientID, creditCard) VALUES
 (3, 5555666677778888);
 
 -- Insertion des films fictifs
-INSERT INTO Movies (movieName, directorName, actorsName, summary) VALUES
-('Interstellar', 'Christopher Nolan', 'Matthew McConaughey, Anne Hathaway, Jessica Chastain', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.');
+INSERT INTO Movies (movieName, directorName, summary) VALUES
+('Interstellar', 'Christopher Nolan', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.');
 
 -- Insertion des locations de films fictives
-INSERT INTO Rents (clientID, movieID, withdrawalDate, paid, returnDate) VALUES
-(1, 1, '2023-01-01', TRUE, '2023-01-10'),
-(2, 1, '2023-02-01', TRUE, '2023-02-15');
+INSERT INTO Rents (clientID, movieID, withdrawalDate, paid, type, returnDate) VALUES
+(1, 1, '2023-01-01', TRUE, 0, '2023-01-10'),
+(2, 1, '2023-02-01', TRUE, 1, '2023-02-15');
 
 -- Insertion des acteurs fictifs
 INSERT INTO Actors (actorName) VALUES
@@ -35,16 +35,16 @@ INSERT INTO MovieActors (movieID, actorID) VALUES
 (1, 3);
 
 -- Insertion de films fictifs
-INSERT INTO Movies (movieName, directorName, actorsName, summary) VALUES
-('The Matrix', 'Lana Wachowski, Lilly Wachowski', 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss', 'A computer hacker learns about the true nature of his reality and his role in the war against its controllers.'),
-('Inception', 'Christopher Nolan', 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', 'A thief who enters the dreams of others to steal their secrets faces a new challenge when tasked with planting an idea into someone''s mind.'),
-('Pulp Fiction', 'Quentin Tarantino', 'John Travolta, Uma Thurman, Samuel L. Jackson', 'Various interconnected stories of criminals in Los Angeles.'),
-('The Shawshank Redemption', 'Frank Darabont', 'Tim Robbins, Morgan Freeman, Bob Gunton', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'),
-('The Dark Knight', 'Christopher Nolan', 'Christian Bale, Heath Ledger, Aaron Eckhart', 'When the menace known as The Joker emerges, Batman must confront him and bring justice to Gotham City.'),
-('Forrest Gump', 'Robert Zemeckis', 'Tom Hanks, Robin Wright, Gary Sinise', 'The life journey of Forrest Gump, a man with a low I.Q. but good intentions.'),
-('The Godfather', 'Francis Ford Coppola', 'Marlon Brando, Al Pacino, James Caan', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'),
-('Star Wars: Episode IV - A New Hope', 'George Lucas', 'Mark Hamill, Harrison Ford, Carrie Fisher', 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire''s world-destroying battle station.'),
-('Titanic', 'James Cameron', 'Leonardo DiCaprio, Kate Winslet, Billy Zane', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.');
+INSERT INTO Movies (movieName, directorName, summary) VALUES
+('The Matrix', 'Lana Wachowski', 'A computer hacker learns about the true nature of his reality and his role in the war against its controllers.'),
+('Inception', 'Christopher Nolan', 'A thief who enters the dreams of others to steal their secrets faces a new challenge when tasked with planting an idea into someone''s mind.'),
+('Pulp Fiction', 'Quentin Tarantino', 'Various interconnected stories of criminals in Los Angeles.'),
+('The Shawshank Redemption', 'Frank Darabont', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'),
+('The Dark Knight', 'Christopher Nolan', 'When the menace known as The Joker emerges, Batman must confront him and bring justice to Gotham City.'),
+('Forrest Gump', 'Robert Zemeckis', 'The life journey of Forrest Gump, a man with a low I.Q. but good intentions.'),
+('The Godfather', 'Francis Ford Coppola', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'),
+('Star Wars: Episode IV - A New Hope', 'George Lucas', 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire''s world-destroying battle station.'),
+('Titanic', 'James Cameron', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.');
 
 -- Ajout des acteurs fictifs
 INSERT INTO Actors (actorName) VALUES
@@ -103,7 +103,7 @@ INSERT INTO MovieActors (movieID, actorID) VALUES
 (9, 26);
 
 -- Ajout des locations de films fictives
-INSERT INTO Rents (clientID, movieID, withdrawalDate, paid, returnDate) VALUES
-(1, 2, '2023-03-01', TRUE, '2023-03-10'),
-(2, 3, '2023-04-01', TRUE, '2023-04-15'),
-(3, 4, '2023-05-01', TRUE, '2023-05-10');
+INSERT INTO Rents (clientID, movieID, withdrawalDate, paid, type, returnDate) VALUES
+(1, 2, '2023-03-01', TRUE,0, '2023-03-10'),
+(2, 3, '2023-04-01', TRUE,0, '2023-04-15'),
+(3, 4, '2023-05-01', TRUE,1, '2023-05-10');
