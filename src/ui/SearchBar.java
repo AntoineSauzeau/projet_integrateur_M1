89bb6042d98a21a4905f3f3af80cd78db15c3b99
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-11-24 10:52:05 +0100

    Utilisation de la barre de recherche

--- src/ui/SearchBar.java
+++ src/ui/SearchBar.java
@@ -2,8 +2,15 @@ package ui;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.io.File;
 import java.io.IOException;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class SearchBar extends JPanel {
 
@@ -19,6 +26,27 @@ public class SearchBar extends JPanel {
 
         bttnSearch = new JLabel(iconSearch);
         tfSearch = new JTextField();
+        // A chaque caractère ajouté dans le textfield, on l'affiche dans la console
+        tfSearch.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                System.out.println(tfSearch.getText());
+                System.out.println("Ajoute");
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                System.out.println(tfSearch.getText());
+                System.out.println("Supprime");
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                System.out.println(tfSearch.getText());
+                System.out.println("Change");
+            }
+        });
+
 
         add(tfSearch);
         add(bttnSearch);
